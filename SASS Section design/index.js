ad4a12e8927ec32ba1c5295c73cee2f30e18f815
Isaac Lemayian <105207068+Lemayian-dev@users.noreply.github.com>
2023-10-27 07:00:20 +0300

    Add files via upload

---
+++ SASS Section design/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+
+    hamburger.addEventListener("click", function () {
+        menu.style.display = menu.style.display === "block" ? "none" : "block";
+    });
+});
